ca8bbb77984ff9f3a3a58f17d993922b66ef04c5
khalilDimassi <dimassi0khalil@gmail.com>
2022-10-12 10:21:57 +0100

    Add project files.

---
+++ AM.ApplicationCore/Services/ServiceFlight.cs
@@ -0,0 +1,128 @@
+﻿using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Services
+{
+    public class ServiceFlight : IServiceFlight
+    {
+        //TP2-Q3: Créer la propriété Flights et l’initialiser à une liste vide
+        public List<Flight> Flights { get; set; } = new List<Flight>();
+        public List<Traveller> Travellers { get; set; } = new List<Traveller>();
+
+        //TP2-Q6: Implémenter la méthode GetFlightDates(string destination)
+        public List<DateTime> GetFlightDates(string destination)
+        {
+            // List<DateTime> ls = new List<DateTime>();
+            // for (int j = 0; j < Flights.Count; j++)
+            //     if (Flights[j].Destination.Equals(destination))
+            //         ls.Add(Flights[j].FlightDate);
+            // return ls;
+
+
+            ////TP2-Q7: Reformuler la requête avec foreach
+            //List<DateTime> ls = new List<DateTime>();
+            //foreach (Flight f in Flights)
+            //    if (f.Destination.Equals(destination))
+            //        ls.Add(f.FlightDate);
+            //return ls;
+
+
+            //TP2-Q9: Reformuler la méthode en utilisant LINQ
+            var query = from f in Flights
+                        where f.Destination.Equals(destination)
+                        select f.FlightDate;
+            return query.ToList();
+        }
+
+
+
+        //TP2-Q8: Implémenter la méthode GetFlights(string filterType, string filterValue)
+        public void GetFlights(string filterType, string filterValue)
+        {
+            switch (filterType)
+            {
+                case "Destination":
+                    foreach (Flight f in Flights)
+                    {
+                        if (f.Destination.Equals(filterValue))
+                            Console.WriteLine(f);
+                    }
+                    break;
+                case "FlightDate":
+                    foreach (Flight f in Flights)
+                    {
+                        if (f.FlightDate == DateTime.Parse(filterValue))
+                            Console.WriteLine(f);
+                    }
+                    break;
+                case "EffectiveArrival":
+                    foreach (Flight f in Flights)
+                    {
+                        if (f.EffectiveArrival == DateTime.Parse(filterValue))
+                            Console.WriteLine(f);
+                    }
+                    break;
+            }
+        }
+
+        public void ShowFlightDetails(Plane plane)
+        {
+            var query = from flight in Flights
+                        where flight.Plane.Equals(plane)
+                        select new
+                        {
+                            flight.FlightDate,
+                            flight.Destination
+                        };
+
+            foreach (var item in query)
+            {
+                System.Console.WriteLine($"\nDate: {item.FlightDate}, Desination: {item.Destination}");
+            }
+        }
+
+        public int ProgrammedFlightNumber(DateTime startDate)
+        {
+            var query = from f in Flights
+                        where (f.FlightDate - startDate).TotalDays < 7 && (f.FlightDate - startDate).TotalDays > 0
+                        select f;
+
+            return query.Count();
+
+        }
+
+        public double DurationAverage(string destination)
+        {
+            var query = from f in Flights
+                        where (f.Destination.Equals(destination))
+                        select f.EstimatedDuration;
+
+            return query.Average();
+
+                    }
+
+        public IEnumerable<Flight> OrderedDurationFlights()
+        {
+            return  from flight in Flights
+                    orderby flight.EstimatedDuration descending
+                    select flight;
+        }
+
+        public IEnumerable<Traveller> SeniorTravellers(Flight flight)
+        {
+
+            var query = from p in flight.Passengers.OfType<Traveller>()
+                        orderby p.BirthDate
+                        select p;
+
+            return query.Take(3);
+        }
+
+
+    }
+}
---
+++ AM.UI.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Services;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        // TP2 - Q5:Créer une instance de la classe ServiceFlight
+        ServiceFlight sf = new ServiceFlight();
+
+        //TP2-Q5:Affecter listFlights à la propriété Flights de la classe ServiceFlight
+        sf.Flights = TestData.listFlights;
+
+        Console.WriteLine("************************************ TESTS AREA  ****************************** ");
+
+        Console.WriteLine("Flight dates to Madrid");
+        foreach (var item in sf.GetFlightDates("Madrid"))
+            Console.WriteLine(item);
+
+        Console.WriteLine("Flight dates Airbus planes");
+        sf.ShowFlightDetails(TestData.Airbusplane);
+        
+
+        Console.WriteLine($"Number of flights up for the next 7 days: {sf.ProgrammedFlightNumber(new DateTime(2021, 12, 30))}");
+
+        Console.WriteLine($"Estimated duration to Paris: {sf.DurationAverage("Paris")}");
+
+        Console.WriteLine("Ordered longest to shortest flights:}");
+        foreach (var item in sf.OrderedDurationFlights()) 
+            Console.WriteLine($"flight: {item}");
+
+        Console.WriteLine("Eldest 3 travellers");
+        foreach (var item in sf.SeniorTravellers(TestData.flight1))
+            Console.WriteLine($"name: {item.FirstName}\ndate of birth: {item.BirthDate}");
+            
+        
+    }
+}
